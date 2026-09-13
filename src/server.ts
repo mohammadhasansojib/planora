ba9016f4f0bfa58e92e4dfabb2817005043d7ffe
mohammadhasansojib <sajibhasan800080@gmail.com>
2026-09-13 16:45:59 +0600

    chore: remove application listen in src/server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,3 @@
 import app from "./app/app.js";
-import { config } from "./app/config/index.js";
 
-const main = () => {
-	const port = config.PORT || 5000;
-
-	app.listen(port, () => {
-		console.log(`Server is running on port ${port}...`);
-	});
-};
-
-main();
+export default app;
